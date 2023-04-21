737054e3da3b6eab2d00adefdc2e4aa32f499f00
Alex Hanna <ahanna0608@gmail.com>
2023-04-21 16:50:13 -0400

    Add files via upload

---
+++ RandomMini.js
@@ -0,0 +1,11 @@
+javascript: (() => {
+    let today = new Date();
+    let year = 2014 + Math.floor(Math.random() * (today.getFullYear() - 2013));
+    let month = Math.floor(Math.random() * 12 + 1);
+    if (year == 2014) month = Math.floor(Math.random() * 5 + 8);
+    else if (year == today.getFullYear()) month = Math.floor(Math.random() * today.getMonth() + 1);
+    let day = Math.floor(Math.random() * new Date(year, month, 0).getDate() + 1);
+    if (year == 2014 && month == 8) day = Math.floor(Math.random() * 11 + 21);
+    else if (year == today.getFullYear() && month == today.getMonth()) day = Math.floor(Math.random() * today.getDay() + 1);
+    window.location.assign("https://www.nytimes.com/crosswords/game/mini/" + year + "/" + month + "/" + day);
+  })();
